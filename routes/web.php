d825827418861187b7b339eb9a5c3b4d7d227682
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-06 18:03:08 +0100

    Add TaskController, Task model and new route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/tasks', [TaskController::class, 'index'])->name('task.index');

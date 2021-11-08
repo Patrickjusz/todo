dce79e7656b7216005829519dce159925d566bdb
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-08 16:19:48 +0100

    Upade enum options in tasks table (state)

---
+++ database/migrations/2021_11_08_151614_change_state_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeStateColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            DB::statement("ALTER TABLE tasks CHANGE COLUMN state state ENUM('active', 'done', 'delete') NOT NULL DEFAULT 'active'");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}

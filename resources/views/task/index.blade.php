99d8d07e315185c4ad288b0c4dfc9880b939fa68
Patrick J <patryk.jastrzebski91@gmail.com>
2021-11-08 22:00:49 +0100

    Add a tag (logo) in index file

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -6,20 +6,21 @@
             <div class="task-list">
                 <div class="row">
                     <div class="col-8">
-                        <h1>Todo <button id="btn-add" class="btn rounded btn-success ">Add</button>
+                        <h1 class="logo"><a href="{{ route('task.index') }}">Todo</a> <button id="btn-add"
+                                class="btn rounded btn-success ">Add</button>
                         </h1>
                     </div>
 
                     <div class="col-4">
                         <div class="input-group rounded">
-                            <input id="search-input" type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
-                                aria-describedby="search-addon" />
+                            <input id="search-input" type="search" class="form-control rounded" placeholder="Search"
+                                aria-label="Search" aria-describedby="search-addon" />
                             <span class="input-group-text border-0" id="search-addon">
                                 <i class="fas fa-search"></i>
                             </span>
                         </div>
                     </div>
-                </div> 
+                </div>
 
                 <div id="render-tasks-wrapper" class="animate__animated animate__fadeIn">
                     {{--  --}}
